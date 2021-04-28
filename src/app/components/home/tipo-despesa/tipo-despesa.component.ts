cc277a08574d30f89cf418db706f17adf2cf7adf
rhayk <rhaykvini@gmail.com>
2021-04-28 20:19:32 -0300

    despesa

---
+++ src/app/components/home/tipo-despesa/tipo-despesa.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+    selector: 'app-tipo-despesa',
+    templateUrl: './tipo-despesa.component.html',
+    styleUrls: ['./tipo-despesa.component.scss'],
+})
+export class TipoDespesaComponent implements OnInit {
+    isLoading: boolean = true;
+
+    constructor(
+
+    ) { }
+
+    ngOnInit(): void {
+
+    }
+
+}
+
+
+
