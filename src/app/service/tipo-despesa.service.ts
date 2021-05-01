a2ec8430184cfd5ffb35610ac1e783ec3f07687a
tony.sousa <tony.sousa@meta.com.br>
2021-05-01 20:47:15 -0300

    Implementa metodo para atualizaçao por id do  tipo despesa

--- src/app/service/tipo-despesa.service.ts
+++ src/app/service/tipo-despesa.service.ts
@@ -6,42 +6,62 @@ import { Page } from '../models/page.model';
 import { TipoDespesa } from '../models/tipo-despesa.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TipoDespesaService {
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  findAll(page: Page<TipoDespesa>, nome: string = null): Observable<Page<TipoDespesa>> {
+  findAll(
+    page: Page<TipoDespesa>,
+    nome: string = null
+  ): Observable<Page<TipoDespesa>> {
     let params = new HttpParams()
-      .append("page", page.number.toString())
-      .append("size", page.size.toString());
+      .append('page', page.number.toString())
+      .append('size', page.size.toString());
 
-    if(nome) {
+    if (nome) {
       params = new HttpParams()
-        .append("page", page.number.toString())
-        .append("size", page.size.toString())
-        .append("nome", nome);
+        .append('page', page.number.toString())
+        .append('size', page.size.toString())
+        .append('nome', nome);
     }
-    
-    return this.http.get<Page<TipoDespesa>>(`${environment.apiUrl}/tipo-despesa/findAllByFilter`, { params });
+
+    return this.http.get<Page<TipoDespesa>>(
+      `${environment.apiUrl}/tipo-despesa/findAllByFilter`,
+      { params }
+    );
   }
 
   isAlreadyExistsByNome(nome: string): Observable<boolean> {
-    const params = new HttpParams().append("nome", nome);
-    return this.http.get<boolean>(`${environment.apiUrl}/tipo-despesa/isAlreadyExistsByNome`, { params });
+    const params = new HttpParams().append('nome', nome);
+    return this.http.get<boolean>(
+      `${environment.apiUrl}/tipo-despesa/isAlreadyExistsByNome`,
+      { params }
+    );
   }
 
   findById(id: number): Observable<TipoDespesa> {
-    return this.http.get<TipoDespesa>(`${environment.apiUrl}/tipo-despesa/${id}`);
+    return this.http.get<TipoDespesa>(
+      `${environment.apiUrl}/tipo-despesa/${id}`
+    );
   }
-  
+
   salvar(nome: string): Observable<TipoDespesa> {
-    return this.http.post<TipoDespesa>(`${environment.apiUrl}/tipo-despesa`, { nome });
+    return this.http.post<TipoDespesa>(`${environment.apiUrl}/tipo-despesa`, {
+      nome,
+    });
   }
 
   deleteById(id: number): Observable<any> {
     return this.http.delete(`${environment.apiUrl}/tipo-despesa/${id}`);
   }
 
+  atualizarById(tipoDespesa: TipoDespesa): Observable<any> {
+    const { id, nome } = tipoDespesa;
+    const url = `${environment.apiUrl}/tipo-despesa/${id}`;
+
+    return this.http.patch(url, {
+      nome,
+    });
+  }
 }
