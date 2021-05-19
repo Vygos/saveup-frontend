8025af407a4b50bcb14f2059ac4c5828fbd1f28d
Victor Gabriel <victorsnt123@gmail.com>
2021-05-18 21:36:12 -0300

    poc de graficos e correçao de libs

--- src/app/components/home/dashboard/dashboard.component.ts
+++ src/app/components/home/dashboard/dashboard.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ChartDataSets, ChartOptions, ChartType } from 'chart.js';
+import { Color, BaseChartDirective, Label } from 'ng2-charts';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss']
+  styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  public lineChartData: ChartDataSets[] = [
+    {
+      data: [700.0, 200.0, 300.23, 1000.5, 3000.55, 950, 1050.23],
+      label: 'Ganhos',
+    },
+    {
+      data: [200.0, 500.0, 55.23, 500.5, 750.55, 950, 1050.23],
+      label: 'Despesas',
+    },
+    {
+      data: [1000.0, 1000.0, 500.23, 500.5, 2300.55, 950, 3250.23],
+      label: 'Saldo Final',
+    },
+  ];
+  public lineChartLabels: Label[] = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+  ];
 
-  constructor() { }
+  public lineChartType: ChartType = 'line';
 
-  ngOnInit(): void {
-  }
+  public lineChartOptions: ChartOptions & { annotation: any } = {
+    responsive: true,
+    scales: {
+      // We use this empty structure as a placeholder for dynamic theming.
+      xAxes: [{}],
+      yAxes: [
+        {
+          id: 'y-axis-0',
+          position: 'left',
+        },
+        {
+          id: 'y-axis-1',
+          position: 'right',
+          gridLines: {
+            color: 'rgba(255,0,0,0.3)',
+          },
+          ticks: {
+            fontColor: 'black',
+          },
+        },
+      ],
+    },
+    annotation: {
+      annotations: [
+        {
+          type: 'line',
+          mode: 'vertical',
+          scaleID: 'x-axis-0',
+          value: 'March',
+          borderColor: 'orange',
+          borderWidth: 2,
+          label: {
+            enabled: true,
+            fontColor: 'orange',
+            content: 'LineAnno',
+          },
+        },
+      ],
+    },
+  };
 
+  lineChartColors: Color[] = [
+    {
+      backgroundColor: 'rgba(148,159,177,0.2)',
+      borderColor: 'rgba(148,159,177,1)',
+      pointBackgroundColor: 'rgba(148,159,177,1)',
+      pointBorderColor: '#fff',
+      pointHoverBackgroundColor: '#fff',
+      pointHoverBorderColor: 'rgba(148,159,177,0.8)',
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {}
 }
--- src/app/components/home/dashboard/dashboard.module.ts
+++ src/app/components/home/dashboard/dashboard.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ChartsModule } from 'ng2-charts';
 
 const route: Routes = [{ path: '', component: DashboardComponent }];
 
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, RouterModule.forChild(route)],
+  imports: [CommonModule, RouterModule.forChild(route), ChartsModule],
 })
 export class DashboardModule {}
