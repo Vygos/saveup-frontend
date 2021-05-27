fe6e7a13bedb37ac66015e3116a0892398b46d64
C1314783 Walber Silva Martins <c1314783@interno.bb.com.br>
2021-05-27 20:14:12 -0300

    adicionado chamadas a api

--- src/app/components/home/apresentacao/apresentacao.component.ts
+++ src/app/components/home/apresentacao/apresentacao.component.ts
@@ -1,5 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Financa } from 'src/app/models/financa.model';
+import { FinancaService } from 'src/app/service/financa.service';
 import { dados } from './apresentacao.mock';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
 @Component({
   selector: 'app-apresentacao',
@@ -10,12 +15,26 @@ export class ApresentacaoComponent implements OnInit {
 
   isLoading: boolean = false;
 
-  dados = dados;
+  dados: Financa[];
 
-  constructor() { }
+  anos: string[];
+  
+  constructor(
+    private financaService: FinancaService,
+    private activatedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.activatedRoute.params.subscribe(params => {
+      const id = params['id'] as number;
 
+      this.financaService.listYears(id).subscribe(anos => {
+        this.anos = anos;
+      })
+
+      this.financaService.findAll(id).subscribe(financas => {
+        this.dados = financas;
+      })
+    })
   }
-
 }
