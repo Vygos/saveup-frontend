7c3c8e27ec31a87447e11211d2c6d170b8394059
C1314783 Walber Silva Martins <c1314783@interno.bb.com.br>
2021-05-27 20:08:07 -0300

    adicionado service financas

---
+++ src/app/service/financa.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Financa } from '../models/financa.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FinancaService {
+
+  constructor(private http: HttpClient) { }
+
+  findAll(id: number): Observable<Financa[]> {
+    return this.http.get<Financa[]>(`${environment.apiUrl}/financa/${id}`);
+  }
+
+  listYears(id: number): Observable<string[]> {
+    return this.http.get<string[]>(`${environment.apiUrl}/financa/listarAnos/${id}`);
+  }
+
+  create(financa: Financa): Observable<Financa> {
+    return this.http.post<Financa>(`${environment.apiUrl}/financa/`, financa);
+  }
+
+}
