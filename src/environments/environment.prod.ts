3f52cecacfe93f994b8b546fae172ae495dd7e8f
Victor Gabriel <victorsnt123@gmail.com>
2021-11-20 15:49:55 -0300

    modificando url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://saveup-dev.eba-cyhjspwf.us-east-2.elasticbeanstalk.com',
-  apiUrlSimple: 'saveup-dev.eba-cyhjspwf.us-east-2.elasticbeanstalk.com',
+  apiUrl: 'http://saveupbackend-env.eba-gyzavz8f.us-east-2.elasticbeanstalk.com/',
+  apiUrlSimple: 'saveupbackend-env.eba-gyzavz8f.us-east-2.elasticbeanstalk.com/',
   email: 'sistema@saveup.com.br',
   pswd: 'saveup-backend',
 };
